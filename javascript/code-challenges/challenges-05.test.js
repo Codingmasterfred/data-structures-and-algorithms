eca4f69b7e76711c643b956e1f3cd7b760f0db69
Codingmasterfred <fredrickg777@gmail.com>
2023-04-30 12:12:25 -0700

    problem 3

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -43,7 +43,9 @@ Write a function named addPurchases that, given an array of objects as input, us
 ------------------------------------------------------------------------------------------------ */
 
 const addPurchases = (arr) => {
-  // Solution code here...
+  return arr.reduce((countUp,Number) =>{
+    return countUp + Number.purchasePrice
+  }, 0)
 };
 
 /* ------------------------------------------------------------------------------------------------
